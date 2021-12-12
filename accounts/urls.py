3fe8c2a3bd98944f9398f4c2fa3ab4ad2f8c8c53
AnanyaR10 <intern_ih_1@inflexionpointbd.com>
2021-12-12 16:30:22 +0600

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 # Create your tests here.
 from django.urls import path
@@ -9,7 +10,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
-  
+
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.landing, name="landing"),
     path('upload_image/', views.image_upload, name="image_upload"),
@@ -18,7 +20,13 @@ urlpatterns = [
     path('adminPage/', views.admin, name="admin"),
     path('logout/', views.log_out, name="log_out"),
     path('create_orders/', views.create_orders, name="create_orders"),
-    path('user/', views.userPage, name="userPage" )
+    path('user/', views.userPage, name="userPage" ),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_rest_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name="password_rest_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+
 ]
 
 if settings.DEBUG:
--- inflexion/settings.py
+++ inflexion/settings.py
@@ -141,3 +141,12 @@ MEDIA_URL = '/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#smtp configuration
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=465
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='intern_IH_1@inflexionpointbd.com'
+EMAIL_HOST_PASSWORD='3auNTp4Ri.gJ'
